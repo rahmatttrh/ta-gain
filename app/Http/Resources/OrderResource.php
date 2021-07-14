7ed0861b29bda6fcfda919547f911bca4f654496
Rahmat Hidayat <rahmattrust@gmail.com>
2021-07-14 11:16:27 +0700

    fix fucking api

--- app/Http/Resources/OrderResource.php
+++ app/Http/Resources/OrderResource.php
@@ -14,12 +14,38 @@ class OrderResource extends JsonResource
      */
     public function toArray($request)
     {
+        if($this->status == "1"){
+            $status = "Drafted Order";
+        } else if($this->status == "2"){
+            $status = "Published Order";
+        } else if($this->status == "3"){
+            $status = "Waiting for the coordinator's response";
+        } else if($this->status == "4"){
+            $status = "Waiting waiting for the delegation to the technician";
+        } else if($this->status == "5"){
+            $status = "On progress";
+        } else if($this->status == "6"){
+            $status = "Reporting on progress";
+        } else if($this->status == "7"){
+            $status = "Finished order";
+        } else if($this->status == "8"){
+            $status = "Waiting for BAST Validation";
+        } else if($this->status == "9"){
+            $status = "Waiting for payment";
+        } else if($this->status == "10"){
+            $status = "Completed order";
+        }
+
         return [
             'id' => $this->id,
+            'status' => $this->status,
+            'info_status' => $status,
             'joborder_id' => $this->joborder_id,
             'client' => $this->pelanggan->nama,
             'client_id' => $this->pelanggan_id,
             'kordinator' => $this->kordinator->nama,
+            'kordinator_id' => $this->kordinator->id,
+            'bast' => $this->bast,
             'teknisi' => $this->teknisis,
             'site' => $this->site,
             'provinsi' => $this->provinsi,
@@ -37,7 +63,8 @@ class OrderResource extends JsonResource
             'modem' => $this->modem,
             'alasan_penolakan' => $this->alasan_penolakan,
             'keterangan_status' => $this->keterangan_status,
-            'published' => $this->created_at->format("d F Y")
+            'created_at' => $this->created_at->format("d F Y"),
+            'updated_at' => $this->updated_at->format("d F Y")
         ];
     }
 
