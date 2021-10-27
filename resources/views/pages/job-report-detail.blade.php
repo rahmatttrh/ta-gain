84bdb7efad822ecf5f3bcb0389542ab78703c7dd
rahmatttrh <rahmattrust@gmail.com>
2021-10-27 11:03:25 +0700

    fixing report url

--- resources/views/pages/job-report-detail.blade.php
+++ resources/views/pages/job-report-detail.blade.php
@@ -75,8 +75,8 @@
                             {{ ($jobphotos->currentpage()-1) * $jobphotos->perpage() + $key + 1 }}
                         </td>
                         <td class="px-4 py-3 text-sm">
-                            <a href="{{ route('report.foto', $job) }}"><img  width="75px" style="cursor: pointer;" class="rounded" src="{{asset('storage/' .$job->foto_pekerjaan)}}" alt=""></a>
-                            {{-- <a href="{{ route('report.foto', $job) }}"><img  width="75px" style="cursor: pointer;" class="rounded" src="{{asset('storage/app/' .$job->foto_pekerjaan)}}" alt=""></a> --}}
+                            {{-- <a href="{{ route('report.foto', $job) }}"><img  width="75px" style="cursor: pointer;" class="rounded" src="{{asset('storage/' .$job->foto_pekerjaan)}}" alt=""></a> --}}
+                            <a href="{{ route('report.foto', $job) }}"><img  width="75px" style="cursor: pointer;" class="rounded" src="{{asset('storage/app/' .$job->foto_pekerjaan)}}" alt=""></a>
                         </td>
                        
                         <td class="px-4 py-3 text-sm">
